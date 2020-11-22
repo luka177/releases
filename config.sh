a50f36eda8598a1a36baa04900ab5f78bb4a4bc7
kiam001 <blablatupfen@gmail.com>
2020-11-22 22:51:37 +0100

    Add Config for Pe+

--- config.sh
+++ config.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
+export device="cedric"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Pixel Experience Plus"
+export ROM_DIR="${WORKSPACE}/peplus"
+export ROM_VERSION="10.0"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/kiam001/manifests/master/kiam001/with_qcom.xml"
+export manifest_url="https://github.com/PixelExperience/manifest"
+export rom_vendor_name="aosp"
+export branch="ten-plus"
 export bacon="bacon"
 export buildtype=""
-export clean=""
+export clean="installclean"
 export upload_recovery=""
 
-export ccache=""
-export ccache_size=""
 
-export jenkins="false"
+export ccache="true"
+export ccache_size="100"
 
-export release_repo=""
+export jenkins="true"
+
+export release_repo="kiam001/releases"
 
 export timezone="UTC"
