a3cb04df78337cebf4d58a7db1bb72fd1a193203
kiam001 <blablatupfen@gmail.com>
2020-11-22 22:49:51 +0100

    Add Config for havoc

--- config.sh
+++ config.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
+export device="cedric"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Havoc OS"
+export ROM_DIR="${WORKSPACE}/havoc"
+export ROM_VERSION="3.11"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/kiam001/manifests/master/kiam001/with_qcom.xml"
+export manifest_url="https://github.com/Havoc-OS/android_manifest"
+export rom_vendor_name="havoc"
+export branch="ten"
 export bacon="bacon"
 export buildtype=""
-export clean=""
+export clean="installclean"
 export upload_recovery=""
 
-export ccache=""
-export ccache_size=""
 
-export jenkins="false"
+export ccache="true"
+export ccache_size="100"
 
-export release_repo=""
+export jenkins="true"
+
+export release_repo="kiam001/releases"
 
 export timezone="UTC"
