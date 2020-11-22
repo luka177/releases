cfbefcbd515aa632112d828d20e60f90383aa09e
kiam001 <blablatupfen@gmail.com>
2020-11-22 22:47:10 +0100

    Add Colt Configs

--- config.sh
+++ config.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
+export device="cedric"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="Colt (Enigma)"
+export ROM_DIR="${WORKSPACE}/colt"
+export ROM_VERSION="v6.5Euphoria"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
-export bacon="bacon"
+export local_manifest_url="https://raw.githubusercontent.com/kiam001/manifests/master/kiam001/with_qcom.xml"
+export manifest_url="https://github.com/Colt-Enigma/platform_manifest"
+export rom_vendor_name="colt"
+export branch="c10"
+export bacon="colt"
 export buildtype=""
-export clean=""
+export clean="installclean"
 export upload_recovery=""
 
-export ccache=""
-export ccache_size=""
 
-export jenkins="false"
+export ccache="true"
+export ccache_size="100"
 
-export release_repo=""
+export jenkins="true"
+
+export release_repo="kiam001/releases"
 
 export timezone="UTC"
