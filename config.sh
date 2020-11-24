d54885400c2ed65947a9c7aa02e084bc5cd53f6a
kiam001 <blablatupfen@gmail.com>
2020-11-24 22:38:38 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-export GITHUB_USER=""
-export GITHUB_EMAIL=""
+export GITHUB_USER="kiam001"
+export GITHUB_EMAIL="blablatupfen@gmail.com"
 
-export device=""
+export device="yggdrasil"
 
-export ROM=""
-export ROM_DIR=""
-export ROM_VERSION=""
+export ROM="lineage"
+export ROM_DIR="${WORKSPACE}/lineage-17.1"
+export ROM_VERSION="17.1"
 export official="false"
-export local_manifest_url=""
-export manifest_url=""
-export rom_vendor_name=""
-export branch=""
+export local_manifest_url="https://raw.githubusercontent.com/kiam001/manifests/master/kiam001/volla_yggdrasil.xml"
+export manifest_url="https://github.com/LineageOS/android"
+export rom_vendor_name="lineage"
+export branch="lineage-17.1"
 export bacon="bacon"
 export buildtype=""
-export clean=""
+export clean="installclean"
 export upload_recovery=""
 
-export ccache=""
-export ccache_size=""
+export ccache="true"
+export ccache_size="100"
 
 export jenkins="false"
 
-export release_repo=""
+export release_repo="kiam001/releases"
 
 export timezone="UTC"
